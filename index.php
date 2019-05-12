5af67102e5d1b2a59174a5d5e900016eebef15f4
viktorderkach777 <viktorderkach777@yandex.ua>
2019-05-12 13:27:30 +0300

    remove comment

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 include_once("config.php");
 
 //fetching data in descending order (lastest entry first)
-//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
 ?>
 
