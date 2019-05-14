bab7d15998dee706c882845a4a3ef94a6947fbae
viktorderkach777 <viktorderkach777@yandex.ua>
2019-05-14 12:36:06 +0300

    redact delete

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 		echo "<td>".$res['name']."</td>";
 		echo "<td>".$res['age']."</td>";
 		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\">Delete</a></td>";		
 	}
 	?>
 	</table>
