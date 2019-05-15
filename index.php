79c09ff9997f3e8e23da89bb9fcf283bb4e9a783
viktorderkach777 <viktorderkach777@yandex.ua>
2019-05-15 22:40:51 +0300

    just test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include_once("config.php");
 //fetching data in descending order (lastest entry first)
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
 ?>
+
 <html>
 <head>	
 	<title>Homepage</title>
